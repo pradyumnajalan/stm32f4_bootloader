0617dc85fd2cba0146f2a7366562e5432515590a
pradyumnajalan <pradumna79@gmail.com>
2026-05-05 16:08:47 +0530

    First Commit: Project Setup

---
+++ Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/pjalan/Bootloader/bootloader/Device/Include/stm32f4xx.h \
+ C:/pjalan/Bootloader/bootloader/Device/Include/stm32f407xx.h \
+ C:/pjalan/Bootloader/bootloader/CMSIS/Include/core_cm4.h \
+ C:/pjalan/Bootloader/bootloader/CMSIS/Include/cmsis_version.h \
+ C:/pjalan/Bootloader/bootloader/CMSIS/Include/cmsis_compiler.h \
+ C:/pjalan/Bootloader/bootloader/CMSIS/Include/cmsis_gcc.h \
+ C:/pjalan/Bootloader/bootloader/CMSIS/Include/mpu_armv7.h \
+ C:/pjalan/Bootloader/bootloader/Device/Include/system_stm32f4xx.h
+C:/pjalan/Bootloader/bootloader/Device/Include/stm32f4xx.h:
+C:/pjalan/Bootloader/bootloader/Device/Include/stm32f407xx.h:
+C:/pjalan/Bootloader/bootloader/CMSIS/Include/core_cm4.h:
+C:/pjalan/Bootloader/bootloader/CMSIS/Include/cmsis_version.h:
+C:/pjalan/Bootloader/bootloader/CMSIS/Include/cmsis_compiler.h:
+C:/pjalan/Bootloader/bootloader/CMSIS/Include/cmsis_gcc.h:
+C:/pjalan/Bootloader/bootloader/CMSIS/Include/mpu_armv7.h:
+C:/pjalan/Bootloader/bootloader/Device/Include/system_stm32f4xx.h:
